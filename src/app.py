5c204eeac21aa4cdea280a41723b891eb974f44a
AntonioRM1231 <jrodriguezm1605@alumno.ipn.mx>
2025-10-25 15:50:56 -0600

    websockets

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-#from flask_socketio import SocketIO, emit, send 
+from flask_socketio import SocketIO, emit, send 
 
 app = Flask(__name__)
 #Clave secreta para websockets
-#app.config['SECRET_KEY'] = 'web_sockets'
-#socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'web_sockets'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -13,6 +13,13 @@ def index():
 @app.route('/connect')
 def connect():
     return render_template('connect.html')
+
+
+@socketio.on('telemetria')
+def telemetria(trama):
+    print(trama)
+    event_name = 'telemetria'
+    emit(event_name,trama, broadcast=True)
 
 if __name__ == '__main__':
     #Usar '0.0.0.0' para que sea accesible en la red local 
