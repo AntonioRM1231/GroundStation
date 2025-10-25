9c5ccb63e5c621548096422fc6fc1d89b0fb3e78
AntonioRM1231 <jrodriguezm1605@alumno.ipn.mx>
2025-10-25 13:08:49 -0600

    commit inicial

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+#from flask_socketio import SocketIO, emit, send 
+
+app = Flask(__name__)
+#Clave secreta para websockets
+#app.config['SECRET_KEY'] = 'web_sockets'
+#socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #Usar '0.0.0.0' para que sea accesible en la red local 
+    app.run(debug=True)
+    #socketio.run(app, debug=True, host='0.0.0.0', port=5000)
