94a05d423944cb1cfa8a424f2f7455a1ce01ca3d
AntonioRM1231 <jrodriguezm1605@alumno.ipn.mx>
2025-10-25 14:09:10 -0600

    jsonvariables

---
+++ src/static/js/main.js
@@ -0,0 +1,13 @@
+/*
+var trama = {
+    "temperatura": 30,
+    "aceleracion": 1.22,
+    "velocidad": 1.33,
+    "altitud": 3.4,
+    "latitud": 99.01,
+    "longitud": -99.34,
+    "apogeo": true,
+    "evento_1": true,
+    "evento_2": false, 
+}
+*/
