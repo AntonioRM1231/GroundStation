61bf37de1419afb2897c04269f78a8cc244beef7
AntonioRM1231 <jrodriguezm1605@alumno.ipn.mx>
2025-11-02 21:24:38 -0600

    prueba

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'web_sockets'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/Estacion_vuelo2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/estacion_prueba'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 socketio = SocketIO(app)
